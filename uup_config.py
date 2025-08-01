2bf199fef66f1947d667137cf8683c6a66fac8ca
Shubham <shubhamydv.578@gmail.com>
2025-08-01 23:19:44 +0530

    Update uup_config.py

--- uup_config.py
+++ uup_config.py
@@ -4,6 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    SOURCE_MONGODB_URI = os.getenv("SOURCE_MONGODB_URI")
+    SOURCE_DB_NAME = os.getenv("SOURCE_DB_NAME")
+    SOURCE_COLLECTION_NAME = os.getenv("SOURCE_COLLECTION_NAME")
+    
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
     
     MODEL_NAME = 'gemini-2.0-flash'
