75c801065f72fb104d7fab723d951adc5e66bd35
Shubham <shubhamydv.578@gmail.com>
2025-08-01 23:18:33 +0530

    Update uup_config.py

--- uup_config.py
+++ uup_config.py
@@ -6,8 +6,5 @@ load_dotenv()
 class Config:
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
     
-    # SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key-here")
-    # MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file size
-    
     MODEL_NAME = 'gemini-2.0-flash'
     EMBEDDING_MODEL = 'sentence-transformers/all-MiniLM-L6-v2'
